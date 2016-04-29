f1d410c3f0b60d2eb26d11b6fafdcb18843139b3
Alex Atmore <acatmore@gmail.com>
2016-04-29 18:11:12 -0500

    slight updates

--- client/Portfolio.js
+++ client/Portfolio.js
@@ -1,23 +1,14 @@
-// if (Meteor.isClient) {
-//   // counter starts at 0
-//   Session.setDefault('counter', 0);
-
-//   Template.hello.helpers({
-//     counter: function () {
-//       return Session.get('counter');
-//     }
-//   });
-
-//   Template.hello.events({
-//     'click button': function () {
-//       // increment the counter when button is clicked
-//       Session.set('counter', Session.get('counter') + 1);
-//     }
-//   });
-// }
-
-// if (Meteor.isServer) {
-//   Meteor.startup(function () {
-//     // code to run on server at startup
-//   });
-// }
+jQuery.ready(function(){ 
+	// $("body").css("opacity", "1"); 
+	$("body").css("background-image","url('https://s3-us-west-2.amazonaws.com/acatmoreimages/background/road-sunset.jpeg')");
+	$("body").css("background-position", "center center");
+	$("body").css("background-size", "cover");
+	$("body").css("background-repeat", "no-repeat");
+	$("body").css("background-attachment", "fixed");
+	// $("body").css("display", "flex");
+	// $("body").css("flex-direction", "column");
+	// $("body").css("margin-top", "175px");
+	$("body").css("z-index", "1");
+	$("body").css("min-width", "1024px");
+	$("body").css("min-height", "768px");
+});
